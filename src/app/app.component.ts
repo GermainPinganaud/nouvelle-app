c808e38c6b9ad4cdd4fb7a9a69b567f5a032baea
GermainPinganaud <germain.pinganaud@gmail.com>
2019-02-21 09:42:25 +0100

    added the component day

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'nouvelle-app';
+  personnes = [
+    { nom :'Einstein', prenom :'Albert', decouverte :'La Relativité',description : "Albert Einstein né le 14 mars 1879 à Ulm, dans le Wurtemberg, et mort le 18 avril 1955 à Princeton, dans le New Jersey, est un physicien théoricien. Il fut successivement allemand, apatride, suisse et de double nationalité helvético-américaine. Il épousa son ancienne camarade d'études Mileva Marić, puis sa cousine Elsa Einstein."},
+    { nom :'Faraday', prenom :'Mickael', decouverte :'Champs Magnétique'},
+    { nom :'Curie', prenom :'Marie', decouverte :'Radioactivité'},
+    { nom :'Curie', prenom :'Pierre', decouverte :'Radioactivité'}];
+  personne = {
+    nom : "Einstein",
+    prenom : "Albert",
+    decouverte : "E=MC2",
+    description : "Einstein présente un parcours scolaire relativement atypique par rapport aux éminents scientifiques qui furent plus tard ses contemporains..."
+  }
+
+  selectionner(personne_cliquee){
+    this.personne = personne_cliquee;
+  }
 }
